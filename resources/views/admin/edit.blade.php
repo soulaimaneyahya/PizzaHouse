7b74f2425e45569b8356e81ffb65e1c2bf8c3903
soulaimaneyahya <soulaimane.yahya@usmba.ac.ma>
2021-11-21 17:47:30 +0100

    edit pizza admin

--- resources/views/admin/edit.blade.php
+++ resources/views/admin/edit.blade.php
@@ -1,17 +1,23 @@
+<style>
+
+</style>
 @extends('layouts.app')
 
 @section('content')
     <div class="container">
         <div class="card">
             <div class="card-header d-flex justify-content-between align-items-center">
-                <h5 class="d-inline">Edit Order #{{$pizzas->id}}</h5>
+                <h5 class="d-inline">Edit Pizza Order #{{$pizzas->id}}</h5>
                 <a href="{{route('admin.index')}}" class="btn btn-secondary">Back</a>
             </div>
             <div class="card-body">          
                 <div class="row justify-content-center">
                     <div class="col-md-8">     
                         <div class="card">
-                            <div class="card-header">{{ __('Pizza details') }}</div>
+                            <div class="card-header d-flex justify-content-between align-items-center">
+                                <h6>Pizza ref #{{$pizzas->id}}</h6>
+                                <h6 class="btn-success px-2 py-1">{{$pizzas->price}}$</h6>
+                            </div>
             
                             <div class="card p-2">
                                 <table class="table table-sm">
@@ -47,7 +53,7 @@
                             </div>
                         </div>
                         <div class="card my-3">
-                            <div class="card-header">Pizza ref #{{$pizzas->id}}</div>
+                            <div class="card-header">{{ __('Pizza details') }}</div>
             
                             <div class="card-body">
                             </div>
@@ -58,6 +64,21 @@
                             <div class="card-header">{{ __('Customer') }}</div>
             
                             <div class="card-body">
+                                <div class="p-1">
+                                    <div class="d-flex justify-content-between align-items-center">
+                                        <span>Customer Informations</span>
+                                        <!-- tooltip -->
+                                        <span class="tl" data-bs-placement="bottom" title="Edit Customer">
+                                            <a href="" class="btn btn-secondary"><i class="bi bi-pencil-square"></i></a>
+                                        </span>
+                                    </div>
+                                    <ul class="CustomerInfos p-3" style="opacity: 0.5;">
+                                        <li>{{$pizzas->name}}</li>
+                                        <li>{{$pizzas->phone}}</li>
+                                        <li>{{$pizzas->city}}</li>
+                                        <li>{{$pizzas->state}}</li>
+                                    </ul>
+                                </div>
                             </div>
                         </div>
                     </div>
